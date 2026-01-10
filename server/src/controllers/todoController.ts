60a571faed4a08cf8cd91f8ba3d0b008177530ac
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2026-01-10 23:09:49 +0800

    Update todoController.ts

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -3,12 +3,29 @@ import TodoModel from "../models/Todo";
 import { errorResponse, successResponse } from "../utils/responseHandler";
 import { CreateTodoRequest, UpdateTodoRequest } from "../types/Todo";
 
-export const getTodos = async (_req: Request, res: Response): Promise<void> => {
+export const getTodos = async (req: Request, res: Response): Promise<void> => {
   try {
-    const startOfDay = new Date();
-    startOfDay.setHours(0, 0, 0, 0);
+    const { date } = req.query;
 
-    const endOfDay = new Date();
+    let startOfDay = new Date();
+    let endOfDay = new Date();
+
+    if (date) {
+      const providedDate = new Date(date as string);
+      if (isNaN(providedDate.getTime())) {
+        errorResponse(
+          res,
+          400,
+          false,
+          "Invalid date format. Use ISO YYYY-MM-DD"
+        );
+        return;
+      }
+      startOfDay = new Date(providedDate);
+      endOfDay = new Date(providedDate);
+    }
+
+    startOfDay.setHours(0, 0, 0, 0);
     endOfDay.setHours(23, 59, 59, 999);
 
     const todos = await TodoModel.find({
