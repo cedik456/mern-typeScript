20d4d6ac792f175b32cd79c355dafb936b58a1af
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2026-01-15 12:12:55 +0800

    Fix api endpoint to renders

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://mern-typescript-6pps.onrender.com/api",
 });
 
 export default api;
