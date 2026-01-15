a3bb2bff39317ea6013d25156163a8bb01a9596a
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2026-01-15 10:31:51 +0800

    Delete DotLoader.tsx

--- client/src/components/DotLoader.tsx
+++
@@ -1,9 +0,0 @@
-export default function DotLoader() {
-  return (
-    <div className="flex items-center justify-center space-x-1">
-      <span className="w-2 h-2 bg-gray-600 rounded-full animate-bounce delay-0"></span>
-      <span className="w-2 h-2 bg-gray-600 rounded-full animate-bounce delay-150"></span>
-      <span className="w-2 h-2 bg-gray-600 rounded-full animate-bounce delay-300"></span>
-    </div>
-  );
-}
